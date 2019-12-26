b332e6ca81c9d4f9c177cd55877474f1b3c9bde1
Fabian R <fabian.rupin@wheelwork.fr>
2019-12-26 22:43:49 +0100

    create translations and begin homepage

---
+++ src/Controller/LodgingController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LodgingController extends AbstractController
+{
+    /**
+     * @Route("/lodging", name="lodging")
+     */
+    public function index()
+    {
+        return $this->render('lodging/index.html.twig', [
+            'controller_name' => 'LodgingController',
+        ]);
+    }
+}
