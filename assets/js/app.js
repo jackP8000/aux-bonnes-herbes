d231a733434ded2ca5d85eb31aef6856b6b583c9
Fabian Rupin <fabian.rupin@wheelwork.fr>
2020-04-20 14:16:40 +0200

    adding homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@ const $ = require('jquery');
 
 require('bootstrap');
 require('../css/app.scss');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
